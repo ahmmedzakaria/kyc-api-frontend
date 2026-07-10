1115944a683def73d6ed715ba9693f5e55d4d0bd
zakaria.ahmmed <zakaria.ahmmed@bracits.com>
2026-07-11 02:09:17 +0600

    layout update ongoing ...

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -12,36 +12,43 @@ export const routes: Routes = [
         children: [
             {
                 path: 'dashboard',
+                data: { title: 'Dashboard', breadcrumb: 'Dashboard' },
                 loadComponent: () =>
                     import('./pages/dashboard/dashboard.component').then(m => m.DashboardComponent),
             },
             {
                 path: 'component-demo',
+                data: { title: 'Component Demo', breadcrumb: 'Component Demo' },
                 loadComponent: () =>
                     import('./pages/component-demo/component-demo.component').then(m => m.ComponentDemoComponent),
             },
             {
                 path: 'person',
+                data: { title: 'Customers', breadcrumb: 'Customers' },
                 loadComponent: () =>
                     import('./pages/person/person-list/person-list.component').then(m => m.PersonListComponent),
             },
             {
                 path: 'person/create',
+                data: { title: 'New Customer', breadcrumb: 'New Customer' },
                 loadComponent: () =>
                     import('./pages/person/person-editor.component').then(m => m.PersonEditorComponent),
             },
             {
                 path: 'person/:id/edit',
+                data: { title: 'Edit Customer', breadcrumb: 'Edit Customer' },
                 loadComponent: () =>
                     import('./pages/person/person-editor.component').then(m => m.PersonEditorComponent),
             },
             {
                 path: 'person/:id/preview',
+                data: { title: 'Customer Details', breadcrumb: 'Customer Details' },
                 loadComponent: () =>
                     import('./pages/person/person-preview.component').then(m => m.PersonPreviewComponent),
             },
             {
                 path: 'kyc',
+                data: { title: 'KYC Cases', breadcrumb: 'KYC Cases' },
                 loadComponent: () =>
                     import('./pages/kyc-list/kyc-list.component').then(m => m.KycListComponent),
             },
